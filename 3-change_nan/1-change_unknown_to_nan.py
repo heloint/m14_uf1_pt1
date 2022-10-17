0592ad7bb983ec1abd846a25626a193438c607f7
heloint <majerdaniel93@gmail.com>
2022-10-17 17:25:24 +0200

    arranged data + 3-change_nan finished (Daniel)

--- 3-change_nan/1-change_unknown_to_nan.py
+++ 3-change_nan/1-change_unknown_to_nan.py
@@ -1,9 +1,8 @@
-from os import wait
 import pandas as pd
 import numpy as np
 
-df_cases: pd.DataFrame = pd.read_csv('../data/cleaned_csv/cases.csv', sep=',')
-df_food : pd.DataFrame = pd.read_csv('../data/cleaned_csv/food.csv', sep=',')
+df_cases: pd.DataFrame = pd.read_csv('../data/cleaned_csv/2-question/cases.csv', sep=',')
+df_food : pd.DataFrame = pd.read_csv('../data/cleaned_csv/2-question/food.csv', sep=',')
 
 # Question 3.1.
 # If the file has no NaN values create some rows with some NaN values.
@@ -16,4 +15,10 @@ df_food.replace('unknown', np.NaN)
 # 2. Now, apply one of these two operations and justify the reason:
 # a) Replace the NaN value of a column with another value. (child feature)
 # ===================================
-df_food['status'].replace(np.NaN, 'unknown')
+df_cases['status'].replace(np.NaN, 'unknown')
+
+
+# Save modified dataframes.
+# ===============================
+df_cases.to_csv('../data/cleaned_csv/3-question/cases.csv', index=False)
+df_food.to_csv('../data/cleaned_csv/3-question/food.csv', index=False)
