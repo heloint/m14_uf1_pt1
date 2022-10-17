12fdc7d367677afabd00d399b306a71abe1ababb
heloint <majerdaniel93@gmail.com>
2022-10-17 16:46:47 +0200

    3-change_nan added (Daniel)

--- 3-change_nan/1-change_unknown_to_nan.py
+++ 3-change_nan/1-change_unknown_to_nan.py
@@ -2,9 +2,18 @@ from os import wait
 import pandas as pd
 import numpy as np
 
-
 df_cases: pd.DataFrame = pd.read_csv('../data/cleaned_csv/cases.csv', sep=',')
-df_food : pd.DataFrame = pd.read_csv('../data/cleaned_csv/cases.csv', sep=',')
+df_food : pd.DataFrame = pd.read_csv('../data/cleaned_csv/food.csv', sep=',')
 
+# Question 3.1.
+# If the file has no NaN values create some rows with some NaN values.
+# ===================================
 df_cases.replace('unknown', np.NaN)
-print(df_)
+df_food.replace('unknown', np.NaN)
+
+
+# Question 3.2.a
+# 2. Now, apply one of these two operations and justify the reason:
+# a) Replace the NaN value of a column with another value. (child feature)
+# ===================================
+df_food['status'].replace(np.NaN, 'unknown')
